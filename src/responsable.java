874fed32d2d7a592b39acb6fbb72807c1d122210
wafamabrouki <147161931+wafamabrouki@users.noreply.github.com>
2023-11-26 22:07:22 +0100

    Delete src directory

--- src/responsable.java
+++
@@ -1,8 +0,0 @@
-public class responsable extends employee{
-    int prime;
-    public responsable(){}
-    public responsable(String nom,String adress,int id,int nbheur,int prime){
-        super( nom,adress, id, nbheur);
-        this.prime=prime;
-    }
-}
--- src/vendeur.java
+++
@@ -1,9 +0,0 @@
-public class vendeur extends employee {
-    int tauxdevente;
-    public vendeur(){}
-    public vendeur(String nom,String adress,int id,int nbheur,int tauxdevente){
-        super(nom,adress, id, nbheur);
-        this.tauxdevente=tauxdevente;
-    }
-
-}
